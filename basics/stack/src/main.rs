b01e252bd47f53b91743a01c3b954be250c3163a
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-05-12 09:50:29 +0900

    add comment

--- basics/stack/src/main.rs
+++ basics/stack/src/main.rs
@@ -4,16 +4,20 @@ fn validate_format(chars: &str) -> bool {
         ('[', ']'),
         ('(', ')')
     ]
-    .iter()
-    .cloned()
+    .into_iter()
     .collect();
 
     let mut stack = Vec::new();
 
     for char in chars.chars() {
         if lookup.contains_key(&char) {
+            // 今後出てくるべき閉じ括弧を push e.g. ( が来たら ) を push して、 ) が来るのを期待する、という設計
             stack.push(lookup[&char]);
+        // 今見ている char が、閉じ括弧のどれか（ }, ], ) ）であるかどうかを判定
         } else if lookup.values().any(|&v| v == char) {
+            // stack.pop() は、最後にスタックに追加された「期待される閉じ括弧」を取り出す。
+            // それが今見ている char と一致するかどうかをチェックする。
+            // 一致しなければ、それは「対応関係が崩れている」ということ。
             if stack.is_empty() || stack.pop() != Some(char) {
                 return false;
             }
