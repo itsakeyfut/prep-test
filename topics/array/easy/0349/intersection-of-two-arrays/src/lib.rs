20871a402abdc46db58fb66645aec57f5190832b
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-06 01:20:58 +0900

    simpler

--- topics/array/easy/0349/intersection-of-two-arrays/src/lib.rs
+++ topics/array/easy/0349/intersection-of-two-arrays/src/lib.rs
@@ -1,36 +1,9 @@
 struct Solution;
 impl Solution {
-    fn insersection(nums1: Vec<i32>, mut nums2: Vec<i32>) -> Vec<i32> {
-        nums2.sort();
-        let search_arr = Self::make_search_fn(&nums2);
-        let mut seen = std::collections::HashSet::new();
-
-        nums1.into_iter()
-            .filter(|&x| search_arr(x) && seen.insert(x))
-            .collect()
-    }
-
-    fn make_search_fn<'a>(arr: &'a[i32]) -> impl Fn(i32) -> bool + 'a {
-        move |target: i32| Self::binary_search(arr, target)
-    }
-
-    fn binary_search(arr: &[i32], target: i32) -> bool {
-        let mut lo: isize = 0;
-        let mut hi: isize = arr.len() as isize - 1;
-
-        while lo <= hi {
-            let mid: isize = lo + (hi - lo) / 2;
-            if arr[mid as usize] == target {
-                return true;
-            }
-            else if arr[mid as usize] < target {
-                lo = mid + 1;
-            }
-            else {
-                hi = mid - 1;
-            }
-        }
-        return false;
+    fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let set1: std::collections::HashSet<_> = nums1.into_iter().collect();
+        let set2: std::collections::HashSet<_> = nums2.into_iter().collect();
+        set1.intersection(&set2).cloned().collect()
     }
 }
 
@@ -44,7 +17,7 @@ mod tests {
         let nums2 = vec![2,2];
         let expected = vec![2];
 
-        assert_eq!(Solution::insersection(nums1, nums2), expected);
+        assert_eq!(Solution::intersection(nums1, nums2), expected);
     }
 
     #[test]
@@ -53,6 +26,6 @@ mod tests {
         let nums2 = vec![9,4,9,8,4];
         let expected = vec![4,9];
 
-        assert_eq!(Solution::insersection(nums1, nums2), expected);
+        assert_eq!(Solution::intersection(nums1, nums2), expected);
     }
 }
